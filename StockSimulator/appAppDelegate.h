154a8566288cc94711fafcefaef78532b11484e4
golfguru1 <golfguru@icloud.com>
2013-09-13 14:34:01 -0400

    Initial commit

---
+++ StockSimulator/appAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  appAppDelegate.h
+//  StockSimulator
+//
+//  Created by Mark Hall on 2013-09-13.
+//  Copyright (c) 2013 Mark Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface appAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StockSimulator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StockSimulator
+//
+//  Created by Mark Hall on 2013-09-13.
+//  Copyright (c) 2013 Mark Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "appAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([appAppDelegate class]));
+    }
+}
